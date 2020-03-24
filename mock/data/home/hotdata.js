4b739feb7881a67c35b10ffe21a6cdeaa4d0f9ab
tangmingheng <mhtang@thoughtworks.com>
2020-03-24 16:51:38 +0800

    修改home页图片地址

--- mock/data/home/hotdata.js
+++ mock/data/home/hotdata.js
@@ -3,25 +3,25 @@ module.exports = {
         {
             id: Math.random().toString().slice(2),
             title: "储物柜",
-            img: "http://www.wwtliu.com/sxtstu/livable/homehot/img_chuwugui.png",
+            img: "http://iwenwiki.com/api/livable/homehot/img_chuwugui.png",
             link: "http://www.iwenwiki.com"
         },
         {
             id: Math.random().toString().slice(2),
             title: "照明灯",
-            img: "http://www.wwtliu.com/sxtstu/livable/homehot/img_zhaoming.png",
+            img: "http://iwenwiki.com/api/livable/homehot/img_zhaoming.png",
             link: "http://www.iwenwiki.com"
         },
         {
             id: Math.random().toString().slice(2),
             title: "抱枕",
-            img: "http://www.wwtliu.com/sxtstu/livable/homehot/img_baozhen.png",
+            img: "http://iwenwiki.com/api/livable/homehot/img_baozhen.png",
             link: "http://www.iwenwiki.com"
         },
         {
             id: Math.random().toString().slice(2),
             title: "镜子",
-            img: "http://www.wwtliu.com/sxtstu/livable/homehot/img_jingzi.png",
+            img: "http://iwenwiki.com/api/livable/homehot/img_jingzi.png",
             link: "http://www.iwenwiki.com"
         }
     ],
@@ -29,25 +29,25 @@ module.exports = {
         {
             id: Math.random().toString().slice(2),
             title: "落地灯",
-            img: "http://www.wwtliu.com/sxtstu/livable/homehot/img_luodideng.png",
+            img: "http://iwenwiki.com/api/livable/homehot/img_luodideng.png",
             link: "http://www.iwenwiki.com"
         },
         {
             id: Math.random().toString().slice(2),
             title: "置物架",
-            img: "http://www.wwtliu.com/sxtstu/livable/homehot/img_zhiwujia.png",
+            img: "http://iwenwiki.com/api/livable/homehot/img_zhiwujia.png",
             link: "http://www.iwenwiki.com"
         },
         {
             id: Math.random().toString().slice(2),
             title: "矮脚灯",
-            img: "http://www.wwtliu.com/sxtstu/livable/homehot/img_aijiaodeng.png",
+            img: "http://iwenwiki.com/api/livable/homehot/img_aijiaodeng.png",
             link: "http://www.iwenwiki.com"
         },
         {
             id: Math.random().toString().slice(2),
             title: "毛巾",
-            img: "http://www.wwtliu.com/sxtstu/livable/homehot/img_maojin.png",
+            img: "http://iwenwiki.com/api/livable/homehot/img_maojin.png",
             link: "http://www.iwenwiki.com"
         }
     ]
